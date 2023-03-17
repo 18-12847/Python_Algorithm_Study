c3fccad768edba81e6f255347debb057ea3904f2
18-12847 <118495919+18-12847@users.noreply.github.com>
2023-03-17 19:24:46 +0900

    파일명 변경 및 실습 코드 추가

---
+++ Do it! 자료구조와 함께 배우는 알고리즘 입문/2. 기본 자료구조와 배열/연결리스트.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def find(self, value):
+        curr_node = self.head
+        while curr_node.value != value:
+            curr_node = curr_node.next
+        return curr_node
+
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def insert(self, node, new_value):
+        new_node = Node(new_value)
+        new_node.next = node.next
+        node.next = new_node
+
+    def remove(self, value):
+        prev_node = self.head
+        while prev_node.next.value != value:
+            prev_node = prev_node.next
+        if prev_node.next is not None:
+            prev_node.next = prev_node.next.next
+
+    def display(self):
+        curr_node = self.head
+        display_string = "["
+        while curr_node is not None:
+            display_string += f"{curr_node.value},"
+            curr_node = curr_node.next
+        display_string = display_string[0:len(display_string) - 1]
+        display_string += "]"
+        print(display_string)
+
+linkedList = SinglyLinkedList()
+linkedList.append(1)
+linkedList.append(2)
+linkedList.append(3)
+linkedList.append(5)
+linkedList.display()
+print(linkedList.find(3))
+linkedList.remove(3)
+linkedList.display()
+linkedList.insert(linkedList.find(2), 10)
+linkedList.display()
