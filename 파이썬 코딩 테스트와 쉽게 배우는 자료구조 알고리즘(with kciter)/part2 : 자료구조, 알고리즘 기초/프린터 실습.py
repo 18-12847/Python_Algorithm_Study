729c54fc50ff17992772eb2cfaa04519c98b97f0
18-12847 <118495919+18-12847@users.noreply.github.com>
2023-03-31 18:45:14 +0900

    프린터 실습 코드 수정

--- 파이썬 코딩 테스트와 쉽게 배우는 자료구조 알고리즘(with kciter)/part2 : 자료구조, 알고리즘 기초/프린터 실습.py
+++ 파이썬 코딩 테스트와 쉽게 배우는 자료구조 알고리즘(with kciter)/part2 : 자료구조, 알고리즘 기초/프린터 실습.py
@@ -6,12 +6,9 @@ def solution(priorities, location):
     cnt = 1
     while True:
         if type(queue[0]) == flag and queue[0] >= max(queue):
-            queue.popleft()
             return cnt
-        elif type(queue[0]) == flag and queue[0] < max(queue):
-            queue.append(queue.popleft())
         elif type(queue[0]) != flag and queue[0] >= max(queue):
             queue.popleft()
             cnt += 1
-        elif type(queue[0]) != flag and queue[0] < max(queue):
+        else:
             queue.append(queue.popleft())
